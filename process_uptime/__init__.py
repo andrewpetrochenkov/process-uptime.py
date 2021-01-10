d8e4d485e6d2ddce344f33b781b725cbd39e01cc
andrewp-as-is <russianidiot.github@gmail.com>
2021-01-10 20:34:13 +0300

    2021:01:10 20:34

--- process_uptime/__init__.py
+++ process_uptime/__init__.py
@@ -1,16 +1,15 @@
-__all__ = ['getstarttime']
+__all__ = ['getstarttime', 'getuptime']
 
 from datetime import datetime
 import os
 import psutil
-import datetime
 
 
 def getstarttime(pid=None):
     if not pid:
         pid = os.getpid()
     p = psutil.Process(pid)
-    return datetime.datetime.fromtimestamp(p.create_time())
+    return datetime.fromtimestamp(p.create_time())
 
 
 def getuptime(pid=None):
